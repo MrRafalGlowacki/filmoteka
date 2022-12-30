06660004350d64c7402f1623b13dc8f54b8626f0
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2022-12-30 21:38:16 +0100

    fetching movies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './add-to-local-storage.js';
+import { getMovies } from './getMovies';
 
 const inputSearch = document.querySelector("input[name='searchQuery']");
+const searchButton = document.querySelector('.search-form__btn');
 
-inputSearch.addEventListener('input', inputListener);
+let currentPage = 1;
 
-async function inputListener() {}
+searchButton.addEventListener('click', inputListener);
+
+async function inputListener(event) {
+  event.preventDefault();
+  currentPage = 1;
+  const searchQuery = inputSearch.value;
+  const moviesList = await getMovies(searchQuery, currentPage);
+  console.log(moviesList);
+}

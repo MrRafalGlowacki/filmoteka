089d82e80c5111ff1a991766d5579b108d80bdfc
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-01-02 23:46:30 +0100

    rendering found movies

---
+++ src/js/searchAndRenderMovies.js
@@ -0,0 +1,38 @@
+import { getMovies } from '../getMovies';
+import { render } from './renderMovieCard';
+
+const main = document.querySelector('.main-box');
+const inputSearch = document.querySelector("input[name='searchQuery']");
+const searchButton = document.querySelector('.search-form__btn');
+
+let currentPage = 1;
+
+searchButton.addEventListener('click', renderFoundMovies);
+
+async function renderFoundMovies(event) {
+  event.preventDefault();
+  currentPage = 1;
+  const searchQuery = inputSearch.value;
+  if (!searchQuery) {
+    alert('Enter movie title');
+  } else {
+    main.innerHTML = '';
+    const moviesList = await getMovies(searchQuery, currentPage);
+    console.log(moviesList);
+    moviesList.results.map(elem => {
+      main.insertAdjacentHTML('beforeend', render(elem));
+    });
+  }
+}
+
+// const renderMovieCard = async () => {
+//   try {
+//     const response = await getMovies();
+//     response.data.results.map(elem => {
+//       main.insertAdjacentHTML('beforeend', render(elem));
+//     });
+//   } catch (error) {
+//     console.log('err ', error);
+//   }
+// };
+export { renderFoundMovies };

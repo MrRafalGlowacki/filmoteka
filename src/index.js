4602a731c6dfd5b24db1fbdd9fe5926f77ba839d
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2023-01-03 21:12:22 +0100

    students added
    first draft

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import './add-to-local-storage.js';
+import './js/students';
 import { renderMovieCard } from './js/getAndRenderTopRatedMovies';
 import { renderFoundMovies } from './js/searchAndRenderMovies.js';
 

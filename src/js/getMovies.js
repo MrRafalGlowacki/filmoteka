7a463ae788171c0be661828e394acb7904ccc8ab
Dominik Banaskiewicz <banaskiewiczdominik@gmail.com>
2023-01-04 22:42:29 +0100

    footer background full width and fixed date in videocard render
    background w footer ma infinite width,
    getMovies.js zmieniłem funkcję aby korzystała z params
    renderMovieCard.js - dodałem funkcję getMovieYear aby renderować tylko rok opublikowania filmu

--- src/js/getMovies.js
+++ src/js/getMovies.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
-const getMovies = async (
-  link = 'https://api.themoviedb.org/3/movie/top_rated?api_key=b942b8bf626a04f48b07153a95ee51a0&language=en-US&page=1'
-) => {
+const API_KEY = 'b942b8bf626a04f48b07153a95ee51a0';
+const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const getMovies = async (pageNumber = 1) => {
   try {
-    const response = await axios.get(link);
+    const response = await axios.get(API_URL, {
+      params: {
+        api_key: `${API_KEY}`,
+        page: pageNumber,
+        language: 'en-US',
+      },
+    });
     return response;
   } catch (error) {
     console.log(error);
   }
 };
 export { getMovies };
+// ?fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads

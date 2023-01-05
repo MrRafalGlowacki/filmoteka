a2c297cd49e47af1707c8ee9f40d6a786447fb2f
Dominik Banaskiewicz <banaskiewiczdominik@gmail.com>
2023-01-05 01:18:10 +0100

    Added spinner
    dodałem spinner-loader przy czekaniu na odpowiedz z  api

--- src/getMovies.js
+++ src/getMovies.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 const API_KEY = 'b942b8bf626a04f48b07153a95ee51a0';
 const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const loader = document.querySelector('.loader');
 
 export async function getMovies(query, pageNumber = 1) {
+  loader.style.display = 'block';
   try {
     const response = await axios.get(API_URL, {
       params: {
@@ -11,8 +13,10 @@ export async function getMovies(query, pageNumber = 1) {
         page: pageNumber,
       },
     });
+    loader.style.display = 'none';
     return response.data;
   } catch (error) {
+    loader.style.display = 'none';
     console.error(error);
   }
 }
--- src/js/getMovies.js
+++ src/js/getMovies.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
-const getMovies = async (
-  link = 'https://api.themoviedb.org/3/movie/top_rated?api_key=b942b8bf626a04f48b07153a95ee51a0&language=en-US&page=1'
-) => {
+const loader = document.querySelector('.loader');
+const API_KEY = 'b942b8bf626a04f48b07153a95ee51a0';
+const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const getMovies = async (pageNumber = 1) => {
+  loader.style.display = 'block';
   try {
-    const response = await axios.get(link);
+    const response = await axios.get(API_URL, {
+      params: {
+        api_key: `${API_KEY}`,
+        page: pageNumber,
+        language: 'en-US',
+      },
+    });
+    loader.style.display = 'none';
     return response;
   } catch (error) {
+    loader.style.display = 'none';
     console.log(error);
   }
 };

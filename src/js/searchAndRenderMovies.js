e0c4f74f2ffb1e66ffb70c2cb1176b233eec6f5e
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2023-01-05 22:23:05 +0100

    Update_2.6
    -Fixed search (enter)
    -added buttonTop

--- src/js/searchAndRenderMovies.js
+++ src/js/searchAndRenderMovies.js
@@ -3,7 +3,8 @@ import { render } from './renderMovieCard';
 
 const main = document.querySelector('.main-box');
 const inputSearch = document.querySelector("input[name='searchQuery']");
-const searchButton = document.querySelector('.icon-search');
+const searchButton = document.querySelector('.header__search-form-btn');
+const toTopBtn = document.querySelector('.to-top');
 
 let currentPage = 1;
 
@@ -33,3 +34,24 @@ async function renderFoundMovies(event) {
 }
 
 export { renderFoundMovies };
+
+
+toTopBtn.addEventListener('click', onTopScroll);
+
+window.addEventListener('scroll', onScrollToTopBtn);
+
+function onScrollToTopBtn() {
+  const offsetTrigger = 100;
+  const pageOffset = window.pageYOffset;
+
+  pageOffset > offsetTrigger
+    ? toTopBtn.classList.remove('is-hidden')
+    : toTopBtn.classList.add('is-hidden');
+}
+
+function onTopScroll() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}

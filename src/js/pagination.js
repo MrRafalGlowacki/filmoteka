06180335446ef42284d0db5fb85a5891d9f8c896
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-01-08 16:13:00 +0100

    protype of hiding pages

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,6 +9,7 @@ let pageCount;
 
 let currentPage;
 
+// TODO: dopisać dodatkową klasę dla pierwszego i ostatniego, żeby ukrywać na małym ekranie
 const appendPageNumber = index => {
   const pageNumber = document.createElement('button');
   pageNumber.className = 'pagination-number';
@@ -60,9 +61,42 @@ const handlePageButtonsStatus = () => {
   }
 };
 
+const displayLessPages = pageNum => {
+  document.querySelectorAll('.pagination-number').forEach(button => {
+    const pageIndex = Number(button.getAttribute('page-index'));
+    // ogólnie cała funkcja ma realizować kilka rzeczy:
+    // ma wyświetlać przyciski x-2 .. x+2, gdzie x to bieżąca strona
+    // ma wyświetlać stronę pierwszą i ostatnią - dla mobilek nie, ale to problem na później
+    //
+
+    // zastanowić się, czy pageCount <=5 nie jest potrzebny przez mobilki
+    if (pageCount <= 7) {
+      // prawdopodobnie dopóki stron jest nie więcej niż 7, nie ma nic do zrobienia
+      // pokazuj wszystkie 7
+    } else {
+      // ppokazuj 1 ... x-2 .. x+2 ... N
+      // dla 1 i N uwzględnij dodatkową klasę, żeby można było ukryć przyciski na mniejszych ekranach
+      // kropki na mniejszych ekranach też są ukryte
+      // trzeba jeszcze obsłużyć gdy x <3 oraz gdy x > N-2
+      if (
+        pageIndex == 1 ||
+        pageIndex == pageCount ||
+        (pageIndex >= pageNum - 2 && pageIndex <= pageNum + 2)
+      ) {
+        // placeholder
+        button.classList.remove('hidden');
+      } else {
+        // placeholder
+        button.classList.add('hidden');
+      }
+    }
+  });
+};
+
 export const setCurrentPage = pageNum => {
   currentPage = pageNum;
 
+  displayLessPages(currentPage);
   handleActivePageNumber();
   handlePageButtonsStatus();
 };

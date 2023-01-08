2d928ef2e9af634cf07f266674ee9f7fc7131301
Dominik Banaskiewicz <banaskiewiczdominik@gmail.com>
2023-01-08 21:17:47 +0100

    reworked

--- src/js/getAndRenderTopRatedMovies.js
+++ src/js/getAndRenderTopRatedMovies.js
@@ -4,15 +4,15 @@ import { getMovies } from './getMovies';
 import { render } from './renderMovieCard';
 
 let genreList;
-const renderMovieCard = async () => {
+const renderMovieCard = async link => {
   try {
     const PromisesArray = [];
-    PromisesArray.push(getMovies());
+    PromisesArray.push(getMovies(link));
     PromisesArray.push(getGenresList());
     const getMoviesAndGenres = await Promise.all(PromisesArray);
     const movies = getMoviesAndGenres[0];
     genreList = getMoviesAndGenres[1];
-
+    main.innerHTML = '';
     movies.data.results.map(elem => {
       main.insertAdjacentHTML('beforeend', render(elem));
     });

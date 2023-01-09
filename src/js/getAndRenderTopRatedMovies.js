b3741224bfc9a78ad1967d9ba251e14d402d08f3
Dominik Banaskiewicz <banaskiewiczdominik@gmail.com>
2023-01-09 01:03:17 +0100

    added pagination on first site and advanced search

--- src/js/getAndRenderTopRatedMovies.js
+++ src/js/getAndRenderTopRatedMovies.js
@@ -2,19 +2,54 @@ const main = document.querySelector('.main-box');
 import { getGenresList } from './getGenreList';
 import { getMovies } from './getMovies';
 import { render } from './renderMovieCard';
+import * as pagination from './pagination';
 
 let genreList;
-const renderMovieCard = async link => {
+let currentPage = 1;
+const renderMovieCard = async (link, pageNumber) => {
   try {
     const PromisesArray = [];
-    PromisesArray.push(getMovies(link));
+    PromisesArray.push(getMovies(link, currentPage));
     PromisesArray.push(getGenresList());
     const getMoviesAndGenres = await Promise.all(PromisesArray);
-    const movies = getMoviesAndGenres[0];
+    let movies = getMoviesAndGenres[0];
     genreList = getMoviesAndGenres[1];
     main.innerHTML = '';
     movies.data.results.map(elem => {
       main.insertAdjacentHTML('beforeend', render(elem));
+    });
+    pagination.getPaginationNumbers(movies.data.total_pages);
+    pagination.setCurrentPage(currentPage);
+    pagination.prevButton.addEventListener('click', async () => {
+      pagination.setCurrentPage(currentPage - 1);
+      movies = await getMovies(link, --currentPage);
+      main.innerHTML = '';
+      movies.data.results.map(elem => {
+        main.insertAdjacentHTML('beforeend', render(elem));
+      });
+    });
+    pagination.nextButton.addEventListener('click', async () => {
+      pagination.setCurrentPage(currentPage + 1);
+      movies = await getMovies(link, ++currentPage);
+      main.innerHTML = '';
+      movies.data.results.map(elem => {
+        main.insertAdjacentHTML('beforeend', render(elem));
+      });
+    });
+    document.querySelectorAll('.pagination-number').forEach(button => {
+      const pageIndex = Number(button.getAttribute('page-index'));
+      if (pageIndex) {
+        button.addEventListener('click', async () => {
+          pagination.setCurrentPage(pageIndex);
+          currentPage = pageIndex;
+
+          movies = await getMovies(link, currentPage);
+          main.innerHTML = '';
+          movies.data.results.map(elem => {
+            main.insertAdjacentHTML('beforeend', render(elem));
+          });
+        });
+      }
     });
   } catch (error) {
     console.log('err ', error);

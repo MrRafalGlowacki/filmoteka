ecfc60220f661125fc063c2a77ad7bb3afad2606
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2023-01-13 11:06:28 +0100

    Update searchAndRenderMovies.js

--- src/js/searchAndRenderMovies.js
+++ src/js/searchAndRenderMovies.js
@@ -8,7 +8,6 @@ const header = document.querySelector('.header__no-movies');
 const main = document.querySelector('.main-box');
 const inputSearch = document.querySelector("input[name='searchQuery']");
 const searchButton = document.querySelector('.header__search-form-btn');
-const genresClear = document.querySelector('.genres-menu__clear')
 // const toTopBtn = document.querySelector('.to-top');
 
 let currentPage = 1;
